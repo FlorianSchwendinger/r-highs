00e0d8c0437d2e6b7740c2ced025bd69bce2f259
Florian Schwendinger <FlorianSchwendinger@gmx.at>
2022-08-15 22:17:43 +0200

    change folder structure

--- inst/build_highs.sh
+++ inst/build_highs.sh
@@ -17,9 +17,17 @@ if test -z "${CMAKE_EXE}"; then
 fi
 
 
+R_MACHINE=`R -s -e 'cat(unname(Sys.info()["machine"]))'`
+
+echo ""
+echo "arch: $(arch)"
+echo "R_MACHINE: ${R_MACHINE}"
+echo ""
+
+
 : ${R_CC=`R CMD config CC`}
 if test "$(uname -s)" = "Darwin"; then
-    if test "$(arch)" = "arm64"; then
+    if test "${R_MACHINE}" = "arm64"; then
         echo "Detected 'darwin' with 'M1'"
         USE_DARWIN_M1="TRUE"
         cp -f inst/patches/CMakeLists_darwin-M1-static.txt inst/HiGHS/CMakeLists.txt
@@ -30,7 +38,7 @@ if test "$(uname -s)" = "Darwin"; then
 else
     echo "Detected non 'Darwin'"
     # Detect the compiler with which R was built.
-    if test "${R_CC}" = "gcc"; then
+    if [[ "${R_CC}" == 'gcc'* ]]; then
         echo "  use GCC"
         R_CC="gcc"
         R_CXX="g++"
@@ -44,10 +52,10 @@ else
 fi
 
 
-R_HIGHS_PKG_DIR=`pwd`
-HIGHS_SRC_DIR=${R_HIGHS_PKG_DIR}/inst/HiGHS
+R_HIGHS_PKG_HOME=`pwd`
+HIGHS_SRC_DIR=${R_HIGHS_PKG_HOME}/inst/HiGHS
 R_HIGHS_BUILD_DIR=${HIGHS_SRC_DIR}/build
-R_HIGHS_LIB_DIR=${R_HIGHS_PKG_DIR}/src/highslib
+R_HIGHS_LIB_DIR=${R_HIGHS_PKG_HOME}/src/highslib
 
 mkdir -p ${R_HIGHS_BUILD_DIR}
 mkdir -p ${R_HIGHS_LIB_DIR}
@@ -62,5 +70,3 @@ else
 fi
 
 ${MAKE} install
-
-cd ${R_HIGHS_PKG_DIR}
