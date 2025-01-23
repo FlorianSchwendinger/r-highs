6e95341f95dc7f13e2a427ad02c190c85e1e67fd
Florian Schwendinger <FlorianSchwendinger@gmx.at>
2025-01-23 14:21:32 +0100

    fix check notes

--- tests/runtime_error.R
+++ tests/runtime_error.R
@@ -1,28 +1,24 @@
 library("highs")
 
-
-# HiGHS/src/util/HighsHashTree.h:858:32: runtime error: index 1 out of bounds for type 'NodePtr [1]'
-writeLines(paste(rep("#", 80), collapse = ""))
-writeLines("Highs runtime error which showed up in an example from the designmatch package")
-writeLines(paste(rep("#", 80), collapse = ""))
+# -----------------------------------------------------------------------------
+#
+# Example from the designmatch package
+# - to reproduce error shown in gccUBSAN checks
+#
+# -----------------------------------------------------------------------------
 file <- system.file("op/distmatch_1.rds", package = "highs")
 op <- readRDS(file)
-# highs_write_model(do.call(highs_model, op[setdiff(names(op), "control")]), "inst/distmatch_1.lp")
 str(op)
 do.call(highs_solve, op)
 
 
-writeLines(paste(rep("#", 80), collapse = ""))
-writeLines("Highs runtime error which showed up in an example from the MatchIt package")
-writeLines(paste(rep("#", 80), collapse = ""))
+# -----------------------------------------------------------------------------
+#
+# Example from the MatchIt package
+# - to reproduce error shown in gccUBSAN checks
+#
+# -----------------------------------------------------------------------------
 file <- system.file("op/matchit_1.rds", package = "highs")
 op <- readRDS(file)
-# highs_write_model(do.call(highs_model, op[setdiff(names(op), "control")]), "inst/matchit_1.lp")
 str(op)
 do.call(highs_solve, op)
-
-
-# model <- do.call(highs_model, op[setdiff(names(op), "control")])
-# control <- do.call(highs_control, op$control)
-# solver <- highs:::new_solver(model)
-# highs:::solver_write_model(solver, "inst/model.lp")
